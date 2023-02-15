d829e5e13c701796804587c5c06a72b0e2a688b3
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-02-15 21:21:04 +0500

    get_max_age_user_name.py

--- get_max_age_user_name.py
+++ get_max_age_user_name.py
@@ -17,10 +17,8 @@ def get_max_age_user_name(data:list) -> str:
     for w in data:
         if s==w['age']:
             d=w['name']
-    x1="'"
-    x2="'"
-    x3=x1+d+x2       
-    return x3
+         
+    return d
 data = [
   {
     'name': 'John', 
